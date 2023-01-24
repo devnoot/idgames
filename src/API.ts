70024c751e8974c6f22fedefb35c168378545779
noot <devnoot@gmail.com>
2023-01-23 22:42:31 -0500

    more rewrite

--- src/API.ts
+++ src/API.ts
@@ -1,94 +1,163 @@
 import { request } from 'undici'
+import {
+    Commands,
+    DbPingResponse,
+    GetContentsResponse,
+    GetDirsResponse,
+    GetFilesResponse,
+    GetParentDirResponse,
+    GetResponse,
+    LatestFilesResponse,
+    LatestVotesResponse,
+    PingResponse,
+    SearchResponse,
+} from './API.types'
 
-export type APIActionType =
-    | 'ping'
-    | 'dbping'
-    | 'about'
-    | 'get'
-    | 'getparentdir'
-    | 'getdirs'
-    | 'getfiles'
-    | 'getcontents'
-    | 'latestvotes'
-    | 'latestfiles'
-    | 'search'
+const defaultBaseUrl = new URL('https://doomworld.com/idgames/api/api.php')
+const defaultOutputType = 'json'
 
-export type APIProps = {
-    action: APIActionType
-    baseUrl?: string
-    outputType?: string
+export const ping = async ({ baseUrl = defaultBaseUrl, outputType = defaultOutputType } = {}): Promise<PingResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.ping)
+    url.searchParams.set('out', outputType)
+    const { body } = await request(url)
+    return await body.json()
 }
 
-export type APIPingResponse = {
-    status: string
+export const dbPing = async ({ baseUrl = defaultBaseUrl, outputType = defaultOutputType } = {}): Promise<DbPingResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.dbping)
+    url.searchParams.set('out', outputType)
+    const { body } = await request(url)
+    return await body.json()
 }
 
-export type APIAboutResponse = {
-    /** Who made this awesome thing possible. */
-    credits: string
-    /** Copyright information regarding this API. */
-    copyright: string
-    /** Additional info. */
-    info: string
+export const about = async ({ baseUrl = defaultBaseUrl, outputType = defaultOutputType } = {}): Promise<DbPingResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.about)
+    url.searchParams.set('out', outputType)
+    const { body } = await request(url)
+    return await body.json()
 }
 
-class API {
-    private _baseUrl: string
-    private _outputType: string
-    constructor({
-        baseUrl = 'https://doomworld.com/idgames/api/api.php',
-        outputType = 'json',
-    }: APIProps) {
-        this._baseUrl = baseUrl
-        this._outputType = outputType
-    }
+export const get = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    id = undefined,
+    file = undefined,
+}): Promise<GetResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.get)
+    url.searchParams.set('out', outputType)
+    if (id) url.searchParams.set('id', id)
+    if (file) url.searchParams.set('file', file)
+    const { body } = await request(url)
+    return await body.json()
+}
 
-    private async _checkResponse(action: APIActionType, statusCode: number) {
-        if (statusCode !== 200) {
-            throw new Error(`${action} failed with status code ${statusCode}`)
-        }
-    }
+export const getParentDir = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    id = undefined,
+    name = undefined,
+}): Promise<GetParentDirResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.get)
+    url.searchParams.set('out', outputType)
+    if (id) url.searchParams.set('id', id)
+    if (name) url.searchParams.set('name', name)
+    const { body } = await request(url)
+    return await body.json()
+}
 
-    private _buildUrl(action: APIActionType) {
-        const url = new URL(this._baseUrl)
-        url.searchParams.set('action', action)
-        url.searchParams.set('out', this._outputType)
-        return url
-    }
+export const getDirs = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    id = undefined,
+    name = undefined,
+}): Promise<GetDirsResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.get)
+    url.searchParams.set('out', outputType)
+    if (id) url.searchParams.set('id', id)
+    if (name) url.searchParams.set('name', name)
+    const { body } = await request(url)
+    return await body.json()
+}
 
-    private async _request(action: APIActionType) {
-        const url = this._buildUrl(action)
-        const { body, statusCode } = await request(url)
-        this._checkResponse(action, statusCode)
-        const json = await body.json()
+export const getFiles = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    id = undefined,
+    name = undefined,
+}): Promise<GetFilesResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.get)
+    url.searchParams.set('out', outputType)
+    if (id) url.searchParams.set('id', id)
+    if (name) url.searchParams.set('name', name)
+    const { body } = await request(url)
+    return await body.json()
+}
 
-        if (action === 'ping' || action === 'dbping') {
-            return {
-                statusCode,
-                action,
-                data: json?.content as APIPingResponse,
-            }
-        } else if (action === 'about') {
-            return {
-                statusCode,
-                action,
-                data: json?.content as APIAboutResponse,
-            }
-        } else if (action === 'get') {
-        } else {
-            console.error(`Action ${action} not implemented.`)
-        }
-    }
+export const getContents = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    id = undefined,
+    name = undefined,
+}): Promise<GetContentsResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.get)
+    url.searchParams.set('out', outputType)
+    if (id) url.searchParams.set('id', id)
+    if (name) url.searchParams.set('name', name)
+    const { body } = await request(url)
+    return await body.json()
+}
 
-    public async ping() {
-        return await this._request('ping')
-    }
+export const latestVotes = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    limit = undefined,
+}): Promise<LatestVotesResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.latestvotes)
+    url.searchParams.set('out', outputType)
+    if (limit) url.searchParams.set('limit', limit)
+    const { body } = await request(url)
+    return await body.json()
+}
 
-    public async dbping() {
-        return await this._request('dbping')
-    }
+export const latestFiles = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    limit = undefined,
+    startid = 0,
+}): Promise<LatestFilesResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.latestfiles)
+    url.searchParams.set('out', outputType)
+    if (limit) url.searchParams.set('limit', limit)
+    if (startid) url.searchParams.set('startid', String(startid))
+    const { body } = await request(url)
+    return await body.json()
+}
 
-    public async about() {
-        return await this._request('about')
-    }
+export const search = async ({
+    baseUrl = defaultBaseUrl,
+    outputType = defaultOutputType,
+    query = undefined,
+    type = undefined,
+    sort = undefined,
+    dir = undefined,
+}): Promise<SearchResponse> => {
+    const url = new URL(baseUrl)
+    url.searchParams.set('action', Commands.search)
+    url.searchParams.set('out', outputType)
+    if (query) url.searchParams.set('query', query)
+    if (type) url.searchParams.set('type', type)
+    if (sort) url.searchParams.set('sort', sort)
+    if (dir) url.searchParams.set('dir', dir)
+    const { body } = await request(url)
+    return await body.json()
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,32 @@
 const assert = require('assert')
-const { IdGamesArchiveAPI } = require('../dist/index')
+const { ping, dbPing, about, get, latestFiles } = require('../dist/API')
 
 describe('API Tests', () => {
     it('ping', async () => {
-        const result = await IdGamesArchiveAPI.ping()
-        assert.equal(result, true)
+        const res = await ping()
+        assert(res.content.status === 'true')
+    })
+
+    it('dbping', async () => {
+        const res = await dbPing()
+        assert(res.content.status === 'true')
+    })
+
+    it('about', async () => {
+        const res = await about()
+        assert(typeof res.content.credits === 'string')
+        assert(typeof res.content.copyright === 'string')
+        assert(typeof res.content.info === 'string')
+    })
+
+    it('get', async () => {
+        const res = await get({ id: '666' })
+        assert((res.content.id = 666))
+        assert(typeof res.content.title === 'string')
+    })
+
+    it('latestfiles', async () => {
+        const res = await latestFiles({ limit: 10 })
+        assert(res.content.file.length === 10)
     })
 })
